8c004ef1f0f6575e552a9bc07e5616a0c367d29e
Eric Klaus <ericklaus16@gmail.com>
2022-12-15 19:54:00 -0300

    Update rmp.c

--- rmp.c
+++ rmp.c
@@ -3,23 +3,22 @@
 int main(){
     int a, b;
     scanf("%d %d", &a, &b);
-    int aBackup = a, bBackup = b;
     int numberOfDivisions = 0, factorsSum = 0;
 
     printf("Russian Multiplication: \n");
-    while(aBackup != 0){
-        if(aBackup % 2 != 0){
-            factorsSum += bBackup;
+    while(a != 0){
+        if(a % 2 != 0){
+            factorsSum += b;
         }
-        printf("%d %d", aBackup, bBackup);
-        if(aBackup % 2 != 0){
-            printf(" %d\n", bBackup);
+        printf("%d %d", a, b);
+        if(a % 2 != 0){
+            printf(" %d\n", b);
         } else {
             printf("\n");
         }
 
-        aBackup /= 2;
-        bBackup *= 2;
+        a /= 2;
+        b *= 2;
         numberOfDivisions += 1;
     }
 
