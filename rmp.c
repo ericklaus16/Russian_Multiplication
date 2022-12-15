2626b9d0d3f4b98334bb5855ba8fa2c8b3e6665d
Eric Klaus <ericklaus16@gmail.com>
2022-12-15 19:53:10 -0300

    Update rmp.c

--- rmp.c
+++ rmp.c
@@ -4,41 +4,25 @@ int main(){
     int a, b;
     scanf("%d %d", &a, &b);
     int aBackup = a, bBackup = b;
-    int numberOfDivisions = 0, numberOfMultiplications = 0, portionSum = 0;
+    int numberOfDivisions = 0, factorsSum = 0;
 
+    printf("Russian Multiplication: \n");
     while(aBackup != 0){
-        aBackup /= 2;
-        numberOfDivisions += 1;
-    }
-    aBackup = a;
-    int vetorA[numberOfDivisions], vetorB[numberOfDivisions], parcelas[numberOfDivisions];
-    vetorA[0] = a;
-    vetorB[0] = b;
-
-    while(numberOfMultiplications != numberOfDivisions){
         if(aBackup % 2 != 0){
-            portionSum += bBackup;
+            factorsSum += bBackup;
         }
+        printf("%d %d", aBackup, bBackup);
         if(aBackup % 2 != 0){
-            parcelas[numberOfMultiplications] = bBackup;
+            printf(" %d\n", bBackup);
+        } else {
+            printf("\n");
         }
 
         aBackup /= 2;
         bBackup *= 2;
-        numberOfMultiplications += 1;
-        vetorA[numberOfMultiplications] = aBackup;
-        vetorB[numberOfMultiplications] = bBackup;
+        numberOfDivisions += 1;
     }
 
-    printf("Russian Multiplication: \n");
-    for(int i = 0; i < numberOfDivisions; i++){
-        printf("%d %d", vetorA[i], vetorB[i]);
+    printf("Sum: %d", factorsSum);
 
-        if(vetorA[i] % 2 != 0){
-            printf(" %d\n", parcelas[i]);
-        } else {
-            printf("\n");
-        }
-    }
-    printf("Sum: %d", portionSum);
 }
